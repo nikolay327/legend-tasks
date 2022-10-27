c6f9bf497205252123e1deffa438bfb542d41f1e
nikolay327 <111442458+nikolay327@users.noreply.github.com>
2022-10-27 11:55:40 +0200

    Add files via upload

--- same_event_waveforms/SameEventVisualizer.py
+++ same_event_waveforms/SameEventVisualizer.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pygama.vis.waveform_browser import WaveformBrowser
 
-def draw(lh5_file: str, channel_map: str, entries: list[int] = [0], channel_mask: list[int] = [], size: tuple = (14,4))-> None:
+def draw(lh5_file: str, channel_map: str, entries: list[int] = [0], channel_mask: list[int] = [], size: tuple = (14,14))-> None:
     
     if not re.findall('.lh5$', lh5_file):
         raise ValueError('lh5_file must be a LH5 file')
@@ -12,8 +12,7 @@ def draw(lh5_file: str, channel_map: str, entries: list[int] = [0], channel_mask
         
     with open(channel_map,'r') as f:
         c_map=json.load(f)['hardware_configuration']['channel_map']
-        
-    plt.rcParams["figure.figsize"] = size
+
     plt.rcParams["figure.facecolor"] = "white"
     plt.rcParams["font.size"] = 12
     
@@ -23,11 +22,21 @@ def draw(lh5_file: str, channel_map: str, entries: list[int] = [0], channel_mask
     to_be_drawn=list(set(browsers.keys())-set(channel_mask))
     
     for entry in entries:
-        clear=True
+        #clear=True
+        fig=plt.figure(figsize=size)
+        ax=fig.subplots(11,3)
         for i in range(len(to_be_drawn)):
-            browsers[to_be_drawn[i]].draw_entry(entry, clear=clear)
-            clear=False
-            if i<len(to_be_drawn)-1:
-                browsers[to_be_drawn[i+1]].set_figure(browsers[to_be_drawn[i]])
-        plt.autoscale(axis='y')
-        plt.show()
+            #browsers[to_be_drawn[i]].draw_entry(entry, clear=False)
+            #clear=False
+            #if i<len(to_be_drawn)-1:
+                #browsers[to_be_drawn[i+1]].set_figure(browsers[to_be_drawn[i]])
+        #plt.autoscale(axis='y')
+            browsers[to_be_drawn[i]].set_figure(fig=fig, ax=ax[i//3,i%3])
+            browsers[to_be_drawn[i]].draw_entry(entry, clear=False)
+            ax[i//3,i%3].set_xticklabels([])
+            ax[i//3,i%3].set_yticklabels([])
+            ax[i//3,i%3].set_xlabel('')
+            ax[i//3,i%3].set_ylabel('')
+        #plt.autoscale(axis='y')
+        #plt.show()
+        fig.show()
